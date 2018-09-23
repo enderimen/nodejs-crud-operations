c2a7d430d96c5f6b8badfd515b85904a8ef5cc9f
Ender İMEN <enderimen@hotmail.com>
2018-09-23 14:07:02 +0300

    Post request

--- app_server/controller/loginController.js
+++ app_server/controller/loginController.js
@@ -1,3 +1,8 @@
 module.exports.index = (req, res) => {
     res.render('login');
 }
+
+module.exports.indexPost = (req, res) => {
+    console.log(req.body);
+    res.render('login');
+}
--- app_server/routes/loginRoute.js
+++ app_server/routes/loginRoute.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var loginController = require('../controller/loginController');
 
 router.get('/', loginController.index);
+router.post('/', loginController.indexPost);
 
 module.exports = router;
